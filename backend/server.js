cb18055222480551a37422dfc0ecb3b0a39cdded
andre godeiro <andrevictor.godeiro@gmail.com>
2022-12-24 18:37:16 -0300

    feat: hide port on env

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const routers = require('./routers.js');
 const database = require('./src/config/config.js');
 require('dotenv').config();
 
+const DB_PORT = process.env.DB_PORT;
+
 const app = express();
 
 app.use(express.json());
 app.use(routers);
 
 
-app.listen(3030, () => console.log("Server running in Port 3030!"));
+app.listen(DB_PORT, () => console.log("Server running in Port 3030!"));
