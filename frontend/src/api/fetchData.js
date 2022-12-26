d403c65f25df5568c004d59b120b8e09573092fb
andre godeiro <andrevictor.godeiro@gmail.com>
2022-12-26 14:05:34 -0300

    feat: fecthData created using axios

---
+++ frontend/src/api/fetchData.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const fetchData = async (id) => {
+  const res = await axios({
+    method: 'get',
+    url: `http://localhost:3030/orders/${id}`
+  })
+  return res.data
+}
+
+export default fetchData
