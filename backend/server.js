93bbfdd5c135b2be599529823412c145c9b6bf01
andre godeiro <andrevictor.godeiro@gmail.com>
2022-12-28 16:08:27 -0300

    build: cors install

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const routers = require('./routers.js');
-const database = require('./src/config/config.js');
 require('dotenv').config();
+const cors = require('cors');
 
 const DB_PORT = process.env.DB_PORT;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routers);
+
 
 
 app.listen(DB_PORT, () => console.log("Server running in Port 3030!"));
